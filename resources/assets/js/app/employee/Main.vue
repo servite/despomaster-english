416d887fc3427834ad89c6786fe7af40f1cdb8e3
Hiren <hiren.iih.13@gmail.com>
2019-07-03 16:41:35 +0530

    text changes

---
+++ resources/assets/js/app/employee/Main.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+    import _ from 'lodash'; 
+    Vue.prototype.trans = string => _.get(window.i18n, string)
+    Vue.prototype.trans1 = (string, args) => {
+        let value = _.get(window.i18n, string);
+
+        _.eachRight(args, (paramVal, paramKey) => {
+            value = _.replace(value, `:${paramKey}`, paramVal);
+        });
+        return value;
+    };
+</script>
+
